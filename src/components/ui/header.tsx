23afedc409002fa01ba2ecab45a5e72658758a48
GustavoNery88 <gusta123sam@gmail.com>
2023-10-31 16:37:20 -0300

    save

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,21 +1,56 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { LogInIcon, MenuIcon, ShoppingCartIcon, PercentIcon, ListOrderedIcon, HomeIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
-    return <Card className="flex items-center justify-between p-[1.875rem]">
-        <Button size="icon" variant="outline">
-            <MenuIcon />
-        </Button>
+    return (
+        <Card className="flex items-center justify-between p-[1.875rem]">
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
 
-        <h1 className="text-lg font-semibold">
-            <span className="text-primary">FSW</span> Store
-        </h1>
+                <SheetContent side="left">
+                    <SheetHeader className="text-left text-lg font-semibold">
+                        Menu
+                    </SheetHeader>
 
-        <Button size="icon" variant="outline">
-            <ShoppingCartIcon />
-        </Button>
-    </Card>
+                    <div className="mt-4 flex flex-col gap-2">
+                        <Button className="w-full justify-start gap-2" variant="outline">
+                            <LogInIcon size={16} />
+                            Fazer Login
+                        </Button>
+
+                        <Button className="w-full justify-start gap-2" variant="outline">
+                            <HomeIcon size={16} />
+                            Inicio
+                        </Button>
+
+                        <Button className="w-full justify-start gap-2" variant="outline">
+                            <PercentIcon size={16} />
+                            Ofertas
+                        </Button>
+
+                        <Button className="w-full justify-start gap-2" variant="outline">
+                            <ListOrderedIcon size={16} />
+                            Catálogo
+                        </Button>
+                    </div>
+                </SheetContent>
+            </Sheet>
+
+            <h1 className="text-lg font-semibold">
+                <span className="text-primary">FSW</span> Store
+            </h1>
+
+            <Button size="icon" variant="outline">
+                <ShoppingCartIcon />
+            </Button>
+        </Card>
+    )
 
 };
 
