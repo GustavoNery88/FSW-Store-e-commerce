6146e68fb283212075094d068f15bf1861359624
GustavoNery88 <gusta123sam@gmail.com>
2023-11-02 16:29:54 -0300

    save

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,6 +22,14 @@ export default async function Home() {
     },
   });
   
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner src="/banner-home-01.png" alt="Até 50% de desconto esse mês" />
@@ -44,6 +52,11 @@ export default async function Home() {
 
       <div>
         <PromoBanner src="/banner-home-03.png" alt="Até 50% de desconto em " />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
       
     </div>
