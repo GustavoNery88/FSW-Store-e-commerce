cdbd6f369ca0d3e52882634efa4aceb284fb359c
GustavoNery88 <gusta123sam@gmail.com>
2023-11-02 16:15:25 -0300

    save

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Categories from './components/categories'
 import { prismaClient } from '@/lib/prisma'
 import ProductList from './components/product-list'
+import SectionTitle from '@/components/ui/section-title'
+import PromoBanner from './components/promo-banner'
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -11,20 +13,34 @@ export default async function Home() {
       }
     }
   })
+
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+  
   return (
     <div>
-      <Image src="/banner-home-01.png" width={0} height={0} className="w-full h-auto p-5" sizes="100vw" alt="Até 50% de desconto esse mês" />
+      <PromoBanner src="/banner-home-01.png" alt="Até 50% de desconto esse mês" />
 
       <div className="mt-8 p-5">
         <Categories />
       </div>
 
       <div className="mt-8">
-        <p className="font-semibold uppercase pl-5 mb-3">Ofertas</p>
+        <SectionTitle className="font-semibold uppercase pl-5 mb-3">Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
-      <Image src="/banner-home-02.png" width={0} height={0} className="w-full h-auto p-5" sizes="100vw" alt="Até 50% de desconto em mouses" />
+      <PromoBanner src="/banner-home-02.png" alt="Até 50% de desconto em mouses" />
+
+      <div className="mt-8">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
     </div>
 
   )
---
+++ src/components/ui/section-title.tsx
@@ -0,0 +1,15 @@
+
+import { ComponentProps } from "react";  
+
+const SectionTitle = ({
+  children,
+  ...props
+}: ComponentProps<"p">) => {
+  return (
+    <p className="font-bold uppercase mb-3 pl-5" {...props}>
+      {children}
+    </p>
+  );
+};
+
+export default SectionTitle;
