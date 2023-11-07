880216f6f904ad405b5a6aab1be3cbd34826218e
GustavoNery88 <gusta123sam@gmail.com>
2023-11-07 14:53:40 -0300

    save

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -6,39 +6,42 @@ import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { Button } from "./button";
+import { ScrollArea } from "./scroll-area";
 
 const Cart = () => {
 
-    const { products, subtotal, total, totalDiscount} = useContext(CartContext);
+  const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
-    return (
+  return (
 
-        <div className="flex flex-col gap-8">
-            <Badge className="w-fit gap-1 border-primary border-2 text-base uppercase px-3 py-[0.375rem]" variant="outline">
-                <ShoppingCartIcon size={16} />
-                Carrinho
-            </Badge>
+    <div className="flex h-full flex-col gap-8">
+      <Badge className="w-fit gap-1 border-primary border-2 text-base uppercase px-3 py-[0.375rem]" variant="outline">
+        <ShoppingCartIcon size={16} />
+        Carrinho
+      </Badge>
 
-            <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
-                <div className="h-full">
-                    <div className="flex h-full flex-col gap-8">
-                        {products.length > 0 ? (
-                            products.map((product) => (
-                                <CartItem
-                                    key={product.id}
-                                    product={computeProductTotalPrice(product as any) as any}
-                                />
-                            ))
-                        ) : (
-                            <p className="text-center font-semibold">
-                                Carrinho vazio. Vamos fazer compras?
-                            </p>
-                        )}
-                    </div>
-                </div>
-            </div>
+       {/* RENDERIZAR OS PRODUTOS */}
+       <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
 
-            <div className="flex flex-col gap-3">
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
           <Separator />
 
           <div className="flex items-center justify-between text-xs lg:text-sm">
@@ -69,13 +72,14 @@ const Cart = () => {
 
           <Button
             className="mt-7 font-bold uppercase"
-            
+
           >
             Finalizar compra
           </Button>
-          </div>
         </div>
-    )
+      )}
+    </div>
+  )
 
 }
 
