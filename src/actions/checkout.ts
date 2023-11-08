69135fc470607bbe394263d63803c5c93ecf7b71
GustavoNery88 <gusta123sam@gmail.com>
2023-11-08 11:00:53 -0300

    save

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -15,6 +15,7 @@ export const createCheckout = async (
     mode: "payment",
     success_url: "http://localhost:3000",
     cancel_url: "http://localhost:3000",
+  
 
     line_items: products.map((product) => {
       return {
